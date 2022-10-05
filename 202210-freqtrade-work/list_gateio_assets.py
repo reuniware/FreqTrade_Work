2494c05403241a48d18a69607fe9b25efa3cef2f
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-10-05 21:42:26 +0200

    Create list_gateio_assets.py

---
+++ 202210-freqtrade-work/list_gateio_assets.py
@@ -0,0 +1,39 @@
+import os
+from datetime import datetime
+import ccxt
+
+# retrieve data for the BTC/USDT pair on Binance
+binance = ccxt.binance()
+ftx = ccxt.gateio()
+
+a = ccxt.exchanges
+# for ex in a:
+#     #print(ex)
+#     exchange_class = getattr(ccxt, ex)
+#     #print(exchange_class)
+#     exchange = exchange_class()
+#     exchange.fetch_tickers()
+
+stop_loop = False
+
+tb = ftx.fetch_tickers()
+# tf = ftx.fetch_tickers()
+
+for ex in a:
+    # print(ex)
+    if ex != "gateio":
+        continue
+    exchange_class = getattr(ccxt, ex)
+    # print(exchange_class)
+    exchange = exchange_class()
+    try:
+        tf = exchange.fetch_tickers()
+    except:
+        # print(ex, "exception")
+        continue
+
+    for lineb in tb.items():
+        symbolb = lineb[1]['symbol']
+        askb = lineb[1]['ask']
+        if symbolb.endswith("/USDT"):
+            print("\"" + symbolb + ":USDT,\",")
