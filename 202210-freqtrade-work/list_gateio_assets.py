1e8ea4f8b699f497742ee9d72a34ef1f6724340a
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-10-06 11:55:04 +0200

    Update list_gateio_assets.py

--- 202210-freqtrade-work/list_gateio_assets.py
+++ 202210-freqtrade-work/list_gateio_assets.py
@@ -1,39 +1,27 @@
-import os
-from datetime import datetime
-import ccxt
+#List all possible assets for trading in futures with freqtrade ;)
+#pip install gate-api
 
-# retrieve data for the BTC/USDT pair on Binance
-binance = ccxt.binance()
-ftx = ccxt.gateio()
+from __future__ import print_function
+import gate_api
+from gate_api.exceptions import ApiException, GateApiException
+# Defining the host is optional and defaults to https://api.gateio.ws/api/v4
+# See configuration.py for a list of all supported configuration parameters.
+configuration = gate_api.Configuration(
+    host = "https://api.gateio.ws/api/v4"
+)
 
-a = ccxt.exchanges
-# for ex in a:
-#     #print(ex)
-#     exchange_class = getattr(ccxt, ex)
-#     #print(exchange_class)
-#     exchange = exchange_class()
-#     exchange.fetch_tickers()
+api_client = gate_api.ApiClient(configuration)
+# Create an instance of the API class
+api_instance = gate_api.FuturesApi(api_client)
+settle = 'usdt' # str | Settle currency
 
-stop_loop = False
-
-tb = ftx.fetch_tickers()
-# tf = ftx.fetch_tickers()
-
-for ex in a:
-    # print(ex)
-    if ex != "gateio":
-        continue
-    exchange_class = getattr(ccxt, ex)
-    # print(exchange_class)
-    exchange = exchange_class()
-    try:
-        tf = exchange.fetch_tickers()
-    except:
-        # print(ex, "exception")
-        continue
-
-    for lineb in tb.items():
-        symbolb = lineb[1]['symbol']
-        askb = lineb[1]['ask']
-        if symbolb.endswith("/USDT"):
-            print("\"" + symbolb + ":USDT\",")
+try:
+    # List all futures contracts
+    api_response = api_instance.list_futures_contracts(settle)
+    #print(api_response)
+    for line in api_response:
+        print("\"" + line.name.replace("_", "/") + ":USDT\",")
+except GateApiException as ex:
+    print("Gate api exception, label: %s, message: %s\n" % (ex.label, ex.message))
+except ApiException as e:
+    print("Exception when calling FuturesApi->list_futures_contracts: %s\n" % e)
